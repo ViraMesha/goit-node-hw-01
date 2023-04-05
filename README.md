6f8c77b90f361177e4997d8da000098a5ed20d80
ViraMesha <wira.mescha@gmail.com>
2023-04-05 23:26:33 +0300

    update README file and add to function try..catch

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # goit-node-hw-01
 
-1. https://ibb.co/rQfgqC0
+1. # Отримуємо і виводимо весь список контактів у вигляді таблиці
 
-2. https://ibb.co/BZQFx0r
+   node index.js --action="list"
+   <a href="https://ibb.co/rQfgqC0"><img src="https://i.ibb.co/Rg41txb/Screenshot-1.png" alt="Screenshot-list"/></a>
 
-3. https://ibb.co/sWDkY9k
+2. # Отримуємо контакт по id
 
-4. https://ibb.co/DY9cS3w
+   node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+   <a href="https://ibb.co/BZQFx0r"><img src="https://i.ibb.co/9T1SLQV/Screenshot-2.png" alt="Screenshot-get" /></a>
+
+3. # Додаємо контакт
+
+   node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+   <a href="https://ibb.co/sWDkY9k"><img src="https://i.ibb.co/jJxBXkB/Screenshot-3.png" alt="Screenshot-add" /></a>
+
+4. # Видаляємо контакт
+   node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+   <a href="https://ibb.co/DY9cS3w"><img src="https://i.ibb.co/G9WYbgC/Screenshot-4.png" alt="Screenshot-remove" /></a>
--- contacts.js
+++ contacts.js
@@ -5,37 +5,53 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
 async function listContacts() {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const contact = contacts.find((contact) => contact.id === contactId);
-  return contact;
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    return contact;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const contactIndex = contacts.findIndex(
-    (contact) => contact.id === contactId
-  );
-  const deletedContact = contacts.splice(contactIndex, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
-  return deletedContact;
+  try {
+    const contacts = await listContacts();
+    const contactIndex = contacts.findIndex(
+      (contact) => contact.id === contactId
+    );
+    const deletedContact = contacts.splice(contactIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
+    return deletedContact;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function addContact(name, email, phone) {
-  const contacts = await listContacts();
-  const contact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  contacts.push(contact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
-  return contact;
+  try {
+    const contacts = await listContacts();
+    const contact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(contact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
+    return contact;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
